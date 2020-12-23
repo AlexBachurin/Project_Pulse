8b44e413ebaac6db48424deeae2e059d2c8b4609
Alex <alexbachwork@gmail.com>
2020-12-23 09:31:08 +0300

    added catalog with working tabs and review section + adaptation for both

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,36 @@ $(document).ready(function () {
     ]
   });
 });
+
+//простой скрипт для табов
+$('ul.catalog__tabs').on('click', 'li:not(catalog__tab_active)', function() {
+  $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active')
+    .eq($(this).index()).addClass('catalog__content_active');
+    //чтобы при переключении между табами открытые табитемы возвращались в исходное состояние
+    //добавляем им обратно класс активности и убираем класс активности у описания
+    $('.catalog-item__list').removeClass('catalog-item__list_active');
+    $('.catalog-item__content').addClass('catalog-item__content_active');
+});
+
+//скрипт для переключения контента в итеме
+$('.catalog-item__link').each(function(i) {
+  $(this)
+    .on('click' , function(e) {
+      e.preventDefault();
+      //переключаем класс активности при клике на кнопку "подробнее" у нужного табитема
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+})
+
+$('.catalog-item__back').each(function(i) {
+  $(this)
+    .on('click' , function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+})
+
